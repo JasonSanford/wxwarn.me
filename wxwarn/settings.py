06aaebe9544083ff0520772d8801e95d2b0cd390
Jason Sanford <jason.sanford@mapmyfitness.com>
2012-11-05 08:24:34 -0700

    Adding dj_database_url

--- wxwarn/settings.py
+++ wxwarn/settings.py
@@ -1,4 +1,7 @@
 # Django settings for wxwarn project.
+import os
+
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,14 +13,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Local time zone for this installation. Choices can be found here:
@@ -150,5 +146,4 @@ LOGGING = {
     }
 }
 
-import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
