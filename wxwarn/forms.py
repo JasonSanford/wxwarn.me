8507199e5192e10962cae70498fd1ee22e40b763
Jason Sanford <jason.sanford@mapmyfitness.com>
2013-04-10 15:59:13 -0600

    sms_number no longer required to change user profile settings. Closes #5.

--- wxwarn/forms.py
+++ wxwarn/forms.py
@@ -4,6 +4,12 @@ from wxwarn.models import UserProfile
 
 
 class UserProfileForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+
+        if 'sms_number' in self.fields:
+            self.fields['sms_number'].required = False
+
     class Meta:
         model = UserProfile
         fields = ('send_email_alerts', 'sms_number', 'send_sms_alerts', 'timezone', )
